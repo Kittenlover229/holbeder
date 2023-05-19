912315b7e98e098e13eb75eab5ccc51c9ffdeb21
Kittenlover229 <artur.roos.artur@gmail.com>
2023-05-19 19:55:19 +0300

    Route empty index page

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, Holbeder!");
+use std::error::Error;
+
+use actix_web::{web, App, HttpResponse, HttpServer};
+use dotenv::dotenv;
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    dotenv()?;
+
+    let port: u16 = dotenv::var("PORT")?.parse()?;
+    let hostname: String = dotenv::var("HOSTNAME")?;
+
+    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
+        .bind((hostname, port))?
+        .run()
+        .await?;
+
+    Ok(())
 }
